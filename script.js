1e71c79354cc4449644d23298fd180af1d98fbbe
Rusydina Novitasari <rnvitas697@gmail.com>
2024-09-09 20:41:35 +0700

    commit ketiga

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 //urutannya dibalik dari belakang kedepan
 
 function nilaiArray(arr) {
-  //   let arr = [2, 3, 4, 5, 6];
-  //   const array = [];
-  for (i = 0; i < arr.length; i++) {
-    return arr[i] * 2;
-  }
+  //cara awal salah , lupa harus pake map kalau mau buat array baru
+  //   for (i = 0; i < arr.length; i++) {
+  //     return arr[i] * 2;
+  //   }
+
+  const Arraykalidua = arr.map((item) => item * 2);
+  return Arraykalidua.reverse();
 }
 
 const number = [1, 2, 3, 5, 7];
