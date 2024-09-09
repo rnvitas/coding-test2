efad2a643c5fc656d461b476de93241fe79b0f56
Rusydina Novitasari <rnvitas697@gmail.com>
2024-09-09 20:30:31 +0700

    commit pertama

---
+++ script.js
@@ -0,0 +1,12 @@
+//inputnya dikali dua
+//urutannya dibalik dari belakang kedepan
+
+function nilaiArray(arr) {
+  //   let arr = [2, 3, 4, 5, 6];
+  //   const array = [];
+  for (i = 0; i < arr.length; i++) {
+    return arr[i] * 2;
+  }
+}
+
+console.log(nilaiArray([10, 20]));
